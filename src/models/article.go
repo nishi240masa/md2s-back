66d2b77801ad38744d1945594fabfedb8afd0644
nishi240masa <masa2401106@gmail.com>
2024-12-08 02:30:29 +0900

    Refactor Article model to update field names and add many-to-many relationship

--- src/models/article.go
+++ src/models/article.go
@@ -4,17 +4,17 @@ import "time"
 
 type Article struct {
     ID          int       `gorm:"primary_key" json:"id"`
-    UserId      UUID    `db:"user_id"`
-	Name  string    `db:"name"`
-	IconURL string    `db:"icon_url"`
-    Title       string    `db:"title"`
-    MainMD      string    `db:"main_md"`
-    SlideMD     *string   `db:"slide_md"`
-    CreatedAt   time.Time `db:"created_at"`
-    UpdatedAt   time.Time `db:"updated_at"`
-    LikeCount   int       `db:"like_count"`
-    Public      bool      `db:"public"`
-    QiitaArticle bool     `db:"qiita_article"`
+    UserId      UUID    `db:"user_id" gorm:"column:user_id" json:"user_id"`
+	Name  string    `db:"name" gorm:"column:name" json:"name"`
+	IconURL string    `db:"icon_url" json:"icon_url"`
+    Title       string    `db:"title" gorm:"column:title" json:"title"`
+    MainMD      string    `db:"main_md" gorm:"column:main_md" json:"main_md"`
+    SlideMD     *string   `db:"slide_md" gorm:"column:slide_md" json:"slide_md"`
+    CreatedAt   time.Time `db:"created_at" gorm:"created_at" json:"created_at"`
+    UpdatedAt   time.Time `db:"updated_at" gorm:"updated_at" json:"updated_at"`
+    LikeCount   int       `db:"like_count" gorm:"column:like_count" json:"like_count"`
+    Public      bool      `db:"public" gorm:"column:public" json:"public"`
+    QiitaArticle bool     `db:"qiita_article" gorm:"column:qiita_article" json:"qiita_article"`
 	Tags		[]Tag     `gorm:"many2many:articletagrelations;"` 
 
 }
