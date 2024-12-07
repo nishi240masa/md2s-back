b41ae04cc17899d99a95bb41b1064eab5436e861
ryota <mc_ryouta3@yahoo.co.jp>
2024-12-07 15:55:19 +0900

    [add] スライド変換用のファイル追加

--- src/router/router.go
+++ src/router/router.go
@@ -20,14 +20,12 @@ func Init() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"}, // ここで特定のオリジンを許可することもできます（例: []string{"http://localhost:3000"})
+		AllowOrigins:     []string{"*"},                                       // ここで特定のオリジンを許可することもできます（例: []string{"http://localhost:3000"})
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // 許可するHTTPメソッド
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // 許可するヘッダー
-		AllowCredentials: true, // クレデンシャルを許可するかどうか
-		MaxAge:           12 * time.Hour, // キャッシュの最大時間
+		AllowCredentials: true,                                                // クレデンシャルを許可するかどうか
+		MaxAge:           12 * time.Hour,                                      // キャッシュの最大時間
 	}))
-
-
 
 	r.GET("/", func(c *gin.Context) {
 
@@ -36,7 +34,6 @@ func Init() {
 		})
 
 	})
-
 
 	// ユーザー
 	users := r.Group("/users")
@@ -48,7 +45,6 @@ func Init() {
 	// 画像
 	imgs := r.Group("/imgs")
 	imgs.POST("/imgs", controllers.UploadImg)
-
 
 	// Qiita
 	qiita := r.Group("/qiita")
@@ -78,6 +74,9 @@ func Init() {
 	likes.POST("/", controllers.CreateLike)
 	likes.DELETE("/:id", controllers.DeleteLike)
 
+	// slide
+	slide := r.Group("/slide")
+	slide.GET("", controllers.GetSlide)
 
 	// 指定されたポートでサーバーを開始
 	if err := r.Run(fmt.Sprintf(":%s", port)); err != nil {
